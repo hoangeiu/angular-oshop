20c91bd09f1cb330a1e18e9050f7b820ed1d8b08
hoangeiu <leminhhoangk3@gmail.com>
2021-05-15 09:08:21 +0700

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyDWw9RW6TO-vaSBv5Tu4s7BA2h9j0aH1io',
+    authDomain: 'oshop1405.firebaseapp.com',
+    projectId: 'oshop1405',
+    storageBucket: 'oshop1405.appspot.com',
+    messagingSenderId: '909114192936',
+    appId: '1:909114192936:web:05ce97ba045163491b711d',
+  },
+};
